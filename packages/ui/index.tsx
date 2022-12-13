2dbe703ac3bf3b6c942a080ba8850b0aca0aac42
Karel Koudelka <k.koudelka01@gmail.com>
2022-12-13 16:37:34 +0100

    fix: lint

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-export * from "./Button";
+export * from "./button/Button";
 export * from "./AlzaText/AlzaText";
 export * from "./src/theme";
 export * from "./colors/ColorTiles";
